dbad9fa57bdd1ce5d3aa932c99813c2242127f8d
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-09-29 15:02:14 +0300

    problem with pox.xml

--- src/main/java/mate/project/onlinebookshop/mapper/BookMapper.java
+++ src/main/java/mate/project/onlinebookshop/mapper/BookMapper.java
@@ -1,9 +1,12 @@
 package mate.project.onlinebookshop.mapper;
 
+import mate.project.onlinebookshop.config.MapperConfig;
 import mate.project.onlinebookshop.dto.BookDto;
 import mate.project.onlinebookshop.dto.CreateBookRequestDto;
 import mate.project.onlinebookshop.model.Book;
+import org.mapstruct.Mapper;
 
+@Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
--- src/main/java/mate/project/onlinebookshop/repository/BookRepository.java
+++ src/main/java/mate/project/onlinebookshop/repository/BookRepository.java
@@ -2,10 +2,11 @@ package mate.project.onlinebookshop.repository;
 
 import java.util.List;
 import java.util.Optional;
+import mate.project.onlinebookshop.exception.DataProcessingException;
 import mate.project.onlinebookshop.model.Book;
 
 public interface BookRepository {
-    Book save(Book book);
+    Book save(Book book) throws DataProcessingException;
 
     List<Book> findAll();
 
